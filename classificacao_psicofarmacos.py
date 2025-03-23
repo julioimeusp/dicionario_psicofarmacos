6a88b3fb2c71046cbc14a96707b2afb49e3e2138
julioimeusp <150378062+julioimeusp@users.noreply.github.com>
2025-03-22 23:09:52 -0300

    Create classificacao_psicofarmacos.py

---
+++ classificacao_psicofarmacos.py
@@ -0,0 +1,116 @@
+classificacao_psicofarmacos = {
+    "Antidepressivos Tricíclicos": [
+        "imipramina", "tofranil",
+        "clomipramina", "anafranil", "clo",
+        "amitriptilina", "trytanol", "amytril", "limbitrol", "protanol",
+        "nortriptilina", "pamelor",
+        "maprotilina", "ludiomil"
+    ],
+    "Inibidores Seletivos da Recaptação de Serotonina": [
+        "fluoxetina", "prozac", "daforin", "verotina", "fluxene", "prozen",
+        "sertralina", "setralina", "zoloft", "serenata", "assert", "tolrest", "dieloft",
+        "paroxetina", "aropax", "pondera", "paxil", "celebrin", "paxan", "paxtrat",  'roxetin',
+        "citalopram", "cipramil", "procimax", "citta", "denyl", "maxapran",
+        "escitalopram", "lexapro", "reconter", "exodus", "espran", "esc", "sedopan", "unitram",
+        "fluvoxamina", "revoc", "luvox"
+    ],
+    "Inibidores Seletivos da Recaptação de Serotonina e Noradrenalina": [
+        "venlafaxina", "efexor", "venlift", "venlaxin", "alenthus",
+        "desvenlafaxina", "pristiq", "desve", "desduo", "desventag", "deller", "elifore", "aviv", "vellana", "andes", "imense",
+        "duloxetina", "cymbalta", "velija", "neulox"
+    ],
+    "Inibidores da Recaptação de Noradrenalina e Dopamina": [
+        "bupropiona", "welbutrin", "zyban", "bup", "zetron", "noradop", 'bupium'
+    ],
+    "Inibidores Seletivos da Recaptação de Noradrenalina": [
+        "reboxetina", "prolift", 
+        'atomoxetina', 'atentah'
+    ],
+    "Inibidores da Monoaminoxidase": [
+        "tranilcipromina", "parnate",
+        "moclobemida", "aurorix"
+    ],
+    "Outros Antidepressivos": [
+        "trazodona", "donaren", "sonic",
+        "mirtazapina", "remeron", "menelat", "razapina",
+        "agomelatina", "valdoxan",  
+        'vortioxetina', 'voextor', 'vurtuoso', 'brintelix',
+        'vilazodona',
+        'esketamina', 'escetamina'  
+    ],
+    "Antipsicóticos": [
+        "clorpromazina", "amplictil", "longactil",  
+        "levomepromazina", "neozine", "levozine", "meprozin", 
+        "flufenazina", "anatensoll", "flufenan", 
+        "haloperidol", "haldol",  
+        "tioridazina", "melleril", "unitidazin",
+        "pimozida", "orap",  
+        "risperidona", "risperdal", "riss", "rispidon", "zargus", "risperidon",
+        "olanzapina", "zyprexa", "axonium", "neupine", "zap", "zopix", 
+        "quetiapina", "seroquel", "neutiapim", "quetiapin", "quetros", "queropax", 
+        "ziprasidona", "geodon", 
+        "aripiprazol", "abilify", "aristab", "arpejo",
+        "sulpirida", "equilid", "dogmatil", "sulpan", 
+        "amisulprida", "socian", 
+        "asenapina", "saphris", 
+        "clozapina", "leponex", "pinazan", 
+        "latuda", 
+        'rexulti', 
+        'neuleptil', 
+        'periciazina'
+    ],
+
+    "Estabilizadores de Humor": [
+        "litio", "carbolitium", 'literata',
+        "vapoico", "valproico", "valproato", "torval", "depakene", "depakine", "depakote",
+        "carbamazepina", "tegretol", 
+        "oxcarbazepina", "trileptal", "oxcarb", "oleptal", 
+        "lamotrigina", "lamictal", "lamitor",
+        "topiramato", "topamax", "amato", "egide"],
+
+    "Benzodiazepinicos": [
+        "diazepam", "valium", "somaplus", "ansilive", "dienpax",
+        "clonazepam", "rivotril", "clonotril", "clopam", "epileptil",
+        "alprazolam", "alprazolan", "frontal", "apraz", "tranquinal", "altrox",
+        "bromazepam", "lexotan", "somalium", "brozepax", "nervium", "novazepan",
+        "lorazepam", "lorax", "lorium", "ansirax", "lorapan",
+        "clobazam", "frisium", "urbanil",
+        "cloxazolam", "olcadil", "elum", "anoxolan",
+        "nitrazepam", "sonebon", "nitrazepol", "nitrapan",
+        "flunitrazepam", "rohypnol", "rohydorm",
+        "flurazepam", "dalmadorm",
+        "midazolam", "dormonid", "dormire", "dormium", "hipnazolam",
+        "clordiazepoxido", "psicosedin", 'Lorazepam'],
+
+    'Antagonistas opioides': [
+	'naloxona', 
+        'naltrexona', 'uninaltrex'],
+
+    "Psicoestimulantes": [
+	'metilfenidato', "ritalina", "medato",  "concerta", "ragione", "foq",
+        'lisdexanfetamina', "venvanse", 'juneve'],
+
+    "Eugeroicos": [
+	'modafinila', "stavigile", 
+        'modafinil'],
+
+    'Anticonvulsivantes': [
+	'pregabalina', 'lyrica', 'dorene', 'konduz',
+        'divalproato', 'divalcon', "valpi",
+        'lacosamida', 
+        'gabapentina', 
+        'levetiracetam'],
+
+    'Azapironas': [
+	'ansitec', 
+        'buspirona'],
+
+    'Hipnóticos': [
+	'zolpidem', 'prysma', 'stilnox', 
+        'eszopiclona',  
+        'ramelteona', 'rozerem'],
+
+    'Outras Medicações': []
+    # Esta categoria é opcional, e pode abranger medicamentos fitoterapicos, antroposóficos e outros que não se encaixam nas categorias acima.
+    # No projeto que deu fruto à esse dicionário, colocou-se termos genéricos de medicamentos como "calmantes", "ansiolíticos" ou "antidepressivos".
+}
